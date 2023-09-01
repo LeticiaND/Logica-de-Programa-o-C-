a71244327569d179d93e6ca8ee0545737d437a14
Leticia <leticiandejesus@icloud.com>
2023-09-01 18:22:42 -0300

    Salvando

---
+++ CombinandoForEif/substituicaoDePalavras.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+std::vector<std::string> split(const char separator[], std::string text) 
+{
+    std::vector<std::string> splitted;
+    std::string word = "";
+
+    for (int i = 0; i <= text.length(); i++) 
+    {   
+
+        char* letter = new char(text[i]);
+
+        if (strcasecmp(letter, separator) != 0 && i != text.length()) 
+        {
+            word += text[i];
+            continue;
+        } 
+
+        splitted.push_back(word);
+        word = "";
+
+    }
+
+    return splitted;
+
+}
+
+int main()
+{
+    //Neste exercício, você vai substituir todas as ocorrências de uma palavra por outra em uma string.
+
+    std::string frase = "Eu vou estudar hoje, e vou estudar amanha";
+    std::string palavra = "estudar";
+    std::string palavraSubstituir = "trabalhar";
+    std::string remontandoFrase = "";
+
+    std::vector<std::string> novasPalavras = split(" ", frase);
+
+    for(int i = 0; i < novasPalavras.size(); i++)
+    {
+        if(novasPalavras[i] == palavra)
+        {
+            remontandoFrase += palavraSubstituir;
+        }else
+        {
+            remontandoFrase += novasPalavras[i];
+        }
+        remontandoFrase += " ";
+    }
+    
+    std::cout << remontandoFrase;
+}
