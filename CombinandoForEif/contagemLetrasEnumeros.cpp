3f66fcfe824a612ee9a8859cb13c70ad49bb47dd
Leticia <leticiandejesus@icloud.com>
2023-10-03 15:51:37 -0300

    Salvando

--- CombinandoForEif/contagemLetrasEnumeros.cpp
+++ CombinandoForEif/contagemLetrasEnumeros.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-int main()
+
+int contagemDeLetrasENumeros(std::string frase)
 {
-    //Neste exercício, você vai criar uma função que conta quantas letras e quantos dígitos (números) existem em uma string.
-
-    std::string frase = "Tenho 29 anos";
-    int quantidadeDeLetras = 0;
     int quantidadeDeNumeros = 0;
-
     for(int i = 0; i <= frase.length(); i++)
     {
         std::string letter;
         letter.push_back(frase[i]);//(push_back) estamos remontando um char para string.
         
-        if(std::atoi(letter.c_str()) == 0){ 
-            quantidadeDeLetras++; //(atoi) so consegue converter string nao char.
-        } else {
+        if(std::atoi(letter.c_str()) != 0){ 
             quantidadeDeNumeros++;
-        }
+        } 
     }
+    return quantidadeDeNumeros;
+}
+int main()
+{
+    //Neste exercício, você vai criar uma função que conta quantas letras e quantos dígitos (números) existem em uma string.
+
+    std::string frase = "Tenho 22 anos";
+    int quantidadeDeNumeros = contagemDeLetrasENumeros(frase);
+    int quantidadeDeLetras = frase.length() - quantidadeDeNumeros;
 
     std::cout << "A frase contém: " << quantidadeDeLetras << " letras" << "\n" << "e " << quantidadeDeNumeros << " numeros";
 
--- CombinandoForEif/numerosUnicos.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    //FAZER!!!!!!!!!!!!!
-    //Imprimir os numeros que nao se repetem.
-}
