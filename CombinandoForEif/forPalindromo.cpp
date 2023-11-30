1d5b4c5226c3fb3ade1d0093299bf4cfb6a8df72
Leticia <leticiandejesus@icloud.com>
2023-11-30 17:04:45 -0300

    salvando codigo

--- CombinandoForEif/forPalindromo.cpp
+++ CombinandoForEif/forPalindromo.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
 
-int main ()
+void palindrome (std::string palavra)
 {
-    //FAZER!!!!!!!!!!
-    //Verificar se uma palavra é um palíndromo. 
-    //Um palíndromo é uma palavra que é igual quando lida da esquerda para a direita e vice-versa.
-    // Escreva um programa que verifique se uma palavra é um palíndromo.
-
-    std::string palavra = "rafael";
-
-    std::string palavraInverso = ""; 
-
+    std::string palavraInverso = "";
     for(int i = palavra.length() -1; i >= 0; i--)
     {
         palavraInverso += palavra[i];
@@ -18,10 +10,20 @@ int main ()
 
     if(palavra == palavraInverso)
     {
-        std::cout << "É um palindromo";
+        std::cout << "É um palindrome";
     }else
     {
-        std::cout << "Nao é um palindromo";
+        std::cout << "Nao é um palindrome";
     }
-
 }
+
+int main ()
+{
+    //Verificar se uma palavra é um palíndromo. 
+    //Um palíndromo é uma palavra que é igual quando lida da esquerda para a direita e vice-versa.
+    // Escreva um programa que verifique se uma palavra é um palíndromo.
+
+    std::string palavra = "tenet";
+    palindrome(palavra);
+}
+
